0df8049e06be32b8dae30ec2eb5d1f6f234faf08
Anderlan Januário <anderlan734@gmail.com>
2025-09-04 17:30:03 -0300

    Update n1_nc4.c

--- n1_nc4.c
+++ n1_nc4.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main (){
+  int idade = 25;
+  int quantidade = 1;
+  float altura = 1.82;
+  double peso = 98.46;
+  char letra = 'A';
+  char nome [20] = "Anderlan";
+  
+
+}
