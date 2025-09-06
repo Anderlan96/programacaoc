7caaf23c88d15c9598e44a755194ddd8e4a059e6
Anderlan Januário <anderlan734@gmail.com>
2025-09-05 22:46:36 -0300

    Create n1_nc5.c

---
+++ n1_nc5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+//Sintase padrão printf
+printf("Olá, mundo!\n");
+//printf("texto com formatação", variavel1, variavel2, ...);
+//printf("Olá, mundo!\n");
+
+}
